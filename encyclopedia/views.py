6998ed889c7e5e07b5cd2cdfa315eeb0c0b94032
Eric Munyi <irerieric@gmail.com>
2024-01-06 11:32:35 +0300

    entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from . import util
 
 
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+def index(request, title):
 
+    content = util.get_entry(title)
+    if content is None:
+        raise Http404("Page Not Found")
+
+    return render(request, "encyclopedia/index.html", {
+        "title": title,
+        "content": content
+    })
