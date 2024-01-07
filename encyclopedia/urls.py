4daa1cab428037dc77f23d32aebf84d8e60e4e76
Eric Munyi <irerieric@gmail.com>
2024-01-07 22:25:58 +0300

    update on search entries

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('wiki/<str:title>/', views.entry_detail, name='entry_detail')
+    path('wiki/<str:title>/', views.entry_detail, name='entry_detail'),
+    path('search/', search_entries, name="search_entries:")
 ]
